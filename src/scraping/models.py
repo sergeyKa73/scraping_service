7ee2547f223e65bc9f0df554959720a39f083178
Sergey <karandeev73@gmail.com>
2022-10-18 21:53:33 +0300

    feat: add files apps

---
+++ src/scraping/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class City(models.Model):
+	name = models.CharField(max_length=50,
+							verbose_name='Название города',
+							unique=True)
+	slug = models.CharField(max_length=50, blank=True, unique=True)
+
+	class Meta:
+		verbose_name='Название города'
+		verbose_name_plural='Название городов'
+
+	def __str__(self):
+		return self.name
+
+
+class Language(models.Model):
+	name = models.CharField(max_length=50,
+							verbose_name='Язык программирования',
+							unique=True)
+	slug = models.CharField(max_length=50, blank=True, unique=True)
+
+	class Meta:
+		verbose_name = 'Язык программирования'
+		verbose_name_plural = 'Языки программирования'
+
+	def __str__(self):
+		return self.name
